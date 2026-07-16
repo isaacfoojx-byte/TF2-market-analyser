7fc92194c3d3d47fdc1e7a8301bac763b6d6aa93
IFOO <isaacfoojx@gmail.com>
2026-07-16 17:39:18 +0800

    Update main.py

--- tf2-market-analyzer/scraper/main.py
+++ tf2-market-analyzer/scraper/main.py
@@ -1,13 +1,18 @@
 from browser import get_driver
 from effect_details import scrape_effect
 from csv_utils import save_csv
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 driver = get_driver()
 
 effects = scrape_effect(driver, "Nebula")
 
-save_csv(effects, "tf2-market-analyzer/data/Nebula.csv")
+save_csv(
+    effects,
+    BASE_DIR / "data" / "Nebula.csv"
+)
 
 print("Done!")
 
